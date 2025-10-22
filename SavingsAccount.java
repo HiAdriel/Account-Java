78deac181290fa2004001a4b71ca9aa0f285d95d
HiAdriel <adriel_tjahja_34rpl@student.smktelkom-mlg.sch.id>
2025-10-22 08:42:41 +0700

    Add files via upload

---
+++ SavingsAccount.java
@@ -0,0 +1,15 @@
+
+public class SavingsAccount {
+    double balance;
+    String name;
+    double interestRate;
+
+    public void displayCustomer() {
+        System.out.println("Customer Name: " + name);
+        System.out.println("Balance: " + balance);
+        System.out.println("Interest Rate: " + interestRate);
+        System.out.println("-------------------------");
+
+    }
+
+}
